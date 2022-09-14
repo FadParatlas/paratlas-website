972029ae7a26f7dac6dd69ad24ca321682eb4db1
Fadhil <81182492+FadParatlas@users.noreply.github.com>
2022-09-14 13:12:13 +0800

    added unity-webgl

--- src/components/pages/Demo.js
+++ src/components/pages/Demo.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
+import "./Demo.css";
 
 
 function Demo() {
-  const { unityProvider } = useUnityContext({
-    loaderUrl: "Build/Presentation.loader.js",
-    dataUrl: "Build/Presentation.data",
-    frameworkUrl: "Build/Presentation.framework.js",
-    codeUrl: "Build/Presentation.wasm",
+  const { unityProvider, loadingProgression, isLoaded } = useUnityContext({
+    loaderUrl: "/Build/Presentation.loader.js",
+    dataUrl: "/Build/Presentation.data",
+    frameworkUrl: "/Build/Presentation.framework.js",
+    codeUrl: "/Build/Presentation.wasm",
   });
   return (
-    <Unity unityProvider={unityProvider} style={{ width: 800, height: 600 }} />
+    <React.Fragment>
+      {!isLoaded && (
+        <p>Loading Application... {Math.round(loadingProgression * 100)}%</p>
+      )}
+      <Unity className="unity-app" unityProvider={unityProvider} style={{ 
+        width: "100vw", 
+        height: "100vh",
+        visibility: isLoaded ? "visible" : "hidden" }} />
+    </React.Fragment>
   );
 }
 
